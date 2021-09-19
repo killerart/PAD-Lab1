9c1c4b33991517a52a8e5636e91bffe590631a05
killerart <theemee.work@gmail.com>
2021-09-19 22:39:36 +0300

    added grpc message broker client implementation

---
+++ MessageBroker.Client/GrpcMessageBrokerClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using MessageBroker.Client.Abstractions;
+using MessageBroker.Client.Models;
+using MessageBroker.Grpc;
+
+namespace MessageBroker.Client {
+    public class GrpcMessageBrokerClient : MessageBrokerClientBase, IDisposable {
+        private readonly AsyncDuplexStreamingCall<Request, Response> _stream;
+        private readonly CancellationTokenSource                     _cancellationTokenSource;
+
+        public GrpcMessageBrokerClient(string host, int port) {
+            var grpcClient = new MessageBrokerService.MessageBrokerServiceClient(new Channel(host, port, ChannelCredentials.Insecure));
+            _cancellationTokenSource = new CancellationTokenSource();
+            _stream                  = grpcClient.Connect(cancellationToken: _cancellationTokenSource.Token);
+        }
+
+        public override void StartListening() {
+            Task.Run(Listen, _cancellationTokenSource.Token);
+        }
+
+        private async Task Listen() {
+            while (await _stream.ResponseStream.MoveNext()) {
+                var response = _stream.ResponseStream.Current;
+                await HandleMessage(response.Topic, response.Message);
+            }
+        }
+
+        public override async ValueTask Subscribe<T>(IMessageHandler<T> messageHandler) {
+            var type  = typeof(T);
+            var sub   = new SubscriptionInfo(type, messageHandler);
+            var topic = type.Name;
+
+            if (!Subscriptions.TryAdd(topic, sub))
+                return;
+
+            await _stream!.RequestStream.WriteAsync(new Request { Command = "SUB", Topic = topic });
+        }
+
+        public override async ValueTask Unsubscribe<T>() {
+            var topic = typeof(T).Name;
+            await Unsubscribe(topic);
+        }
+
+        private async ValueTask Unsubscribe(string topic) {
+            if (!Subscriptions.TryRemove(topic, out _)) {
+                return;
+            }
+
+            await _stream.RequestStream.WriteAsync(new Request { Command = "UNSUB", Topic = topic });
+        }
+
+        public override async ValueTask Publish<T>(T message) {
+            var topic = typeof(T).Name;
+            var json  = JsonSerializer.Serialize(message);
+
+            await _stream.RequestStream.WriteAsync(new Request { Command = "PUB", Topic = topic, Message = json });
+        }
+
+        public override async ValueTask Disconnect() {
+            Subscriptions.Clear();
+
+            await _stream.RequestStream.WriteAsync(new Request { Command = "DISCONNECT", Topic = "" });
+            await _stream.RequestStream.CompleteAsync();
+        }
+
+        public void Dispose() {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+            _stream.Dispose();
+        }
+    }
+}
--- MessageBroker.Client/SocketMessageBrokerClient.cs
+++ MessageBroker.Client/SocketMessageBrokerClient.cs
@@ -8,67 +8,52 @@ using MessageBroker.Client.Abstractions;
 using MessageBroker.Client.Models;
 
 namespace MessageBroker.Client {
-    public class SocketMessageBrokerClient : IMessageBrokerClient, IDisposable {
-        private readonly TcpClient                            _socket;
-        private readonly Dictionary<string, SubscriptionInfo> _subscriptions;
+    public class SocketMessageBrokerClient : MessageBrokerClientBase, IDisposable {
+        private readonly TcpClient _socket;
 
         public SocketMessageBrokerClient(string host, int port) {
-            _socket        = new TcpClient(host, port);
-            _subscriptions = new Dictionary<string, SubscriptionInfo>();
+            _socket = new TcpClient(host, port);
         }
 
-        public void StartListening() {
-            Task.Run(async () => {
-                var stream = _socket.GetStream();
-                var reader = new StreamReader(stream);
-                while (true) {
-                    var firstLine = (await reader.ReadLineAsync())?.Split();
-                    if (firstLine is null) {
-                        break;
-                    }
+        public override void StartListening() {
+            Task.Run(Listen);
+        }
 
-                    var command = firstLine[0];
-                    if (command != "EVENT") {
-                        continue;
-                    }
-
-                    var topic               = firstLine[1];
-                    var contentLengthHeader = (await reader.ReadLineAsync())!.ToLower();
-                    await reader.ReadLineAsync();
-                    var length    = int.Parse(contentLengthHeader.Split("content-length: ", StringSplitOptions.RemoveEmptyEntries)[0]);
-                    var charArray = new char[length];
-                    await reader.ReadAsync(charArray);
-                    await reader.ReadLineAsync();
-
-                    if (!_subscriptions.TryGetValue(topic, out var subscription))
-                        continue;
-
-                    var json    = new string(charArray);
-                    var message = JsonSerializer.Deserialize(json, subscription.MessageType);
-                    if (message is null)
-                        continue;
-
-                    var handlerType  = typeof(IMessageHandler<>).MakeGenericType(subscription.MessageType);
-                    var handleMethod = handlerType.GetMethod(nameof(IMessageHandler<object>.HandleAsync));
-                    if (handleMethod is null)
-                        continue;
-
-                    await (ValueTask)handleMethod.Invoke(subscription.Handler,
-                                                         new[] {
-                                                             message
-                                                         })!;
+        private async Task Listen() {
+            var stream = _socket.GetStream();
+            var reader = new StreamReader(stream);
+            while (true) {
+                var firstLine = (await reader.ReadLineAsync())?.Split();
+                if (firstLine is null) {
+                    break;
                 }
-            });
+
+                var command = firstLine[0];
+                if (command != "EVENT") {
+                    continue;
+                }
+
+                var topic               = firstLine[1];
+                var contentLengthHeader = (await reader.ReadLineAsync())!.ToLower();
+                await reader.ReadLineAsync();
+                var length    = int.Parse(contentLengthHeader.Split("content-length: ", StringSplitOptions.RemoveEmptyEntries)[0]);
+                var charArray = new char[length];
+                await reader.ReadAsync(charArray);
+                await reader.ReadLineAsync();
+                var json = new string(charArray);
+
+                await HandleMessage(topic, json);
+            }
         }
 
-        public async ValueTask Subscribe<T>(IMessageHandler<T> messageHandler) {
+        public override async ValueTask Subscribe<T>(IMessageHandler<T> messageHandler) {
             var type   = typeof(T);
             var sub    = new SubscriptionInfo(type, messageHandler);
             var stream = _socket.GetStream();
             var writer = new StreamWriter(stream);
 
             var topic = type.Name;
-            if (!_subscriptions.TryAdd(topic, sub))
+            if (!Subscriptions.TryAdd(topic, sub))
                 return;
 
             await writer.WriteLineAsync($"SUB {topic}");
@@ -76,7 +61,7 @@ namespace MessageBroker.Client {
             await writer.FlushAsync();
         }
 
-        public async ValueTask Unsubscribe<T>() {
+        public override async ValueTask Unsubscribe<T>() {
             var topic = typeof(T).Name;
             await Unsubscribe(topic);
         }
@@ -84,7 +69,7 @@ namespace MessageBroker.Client {
         private async ValueTask Unsubscribe(string topic) {
             var stream = _socket.GetStream();
             var writer = new StreamWriter(stream);
-            if (!_subscriptions.Remove(topic)) {
+            if (!Subscriptions.Remove(topic, out _)) {
                 return;
             }
 
@@ -93,7 +78,7 @@ namespace MessageBroker.Client {
             await writer.FlushAsync();
         }
 
-        public async ValueTask Publish<T>(T message) {
+        public override async ValueTask Publish<T>(T message) {
             var stream = _socket.GetStream();
             var writer = new StreamWriter(stream);
             var topic  = typeof(T).Name;
@@ -106,7 +91,8 @@ namespace MessageBroker.Client {
             await writer.FlushAsync();
         }
 
-        public async ValueTask Disconnect() {
+        public override async ValueTask Disconnect() {
+            Subscriptions.Clear();
             var stream = _socket.GetStream();
             var writer = new StreamWriter(stream);
             await writer.WriteLineAsync("DISCONNECT");
