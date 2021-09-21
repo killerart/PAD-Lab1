dddba74adc02edd3c9c314670327e42b02be804f
killerart <theemee.work@gmail.com>
2021-09-22 02:26:55 +0300

    fixed socket server memory leak

--- MessageBroker.Server/ConnectionManager/Socket/SocketConnectionManager.cs
+++ MessageBroker.Server/ConnectionManager/Socket/SocketConnectionManager.cs
@@ -23,62 +23,61 @@ namespace MessageBroker.Server.ConnectionManager.Socket {
 
         public void Start() {
             _socket.Start();
-            _socket.BeginAcceptTcpClient(HandleConnection, _socket);
             Console.WriteLine($"Socket listening on port {_port}");
-        }
-
-        private void HandleConnection(IAsyncResult result) {
-            _socket.BeginAcceptTcpClient(HandleConnection, _socket);
-
             Task.Run(async () => {
-                using var client = _socket.EndAcceptTcpClient(result);
-                // Console.WriteLine("Client connected");
-                await using var stream = client.GetStream();
-                using var       reader = new StreamReader(stream);
-                await using var writer = new StreamWriter(stream) {
-                    AutoFlush = true
-                };
+                while (true) {
+                    var client = await _socket.AcceptTcpClientAsync();
+                    Task.Run(async () => {
+                        // Console.WriteLine("Client connected");
+                        await using var stream = client.GetStream();
+                        using var       reader = new StreamReader(stream);
+                        await using var writer = new StreamWriter(stream) {
+                            AutoFlush = true
+                        };
 
-                try {
-                    while (true) {
-                        var firstLine = await reader.ReadLineAsync();
+                        try {
+                            while (true) {
+                                var firstLine = await reader.ReadLineAsync();
 
-                        var args = firstLine!.Split();
+                                var args = firstLine!.Split();
 
-                        var command = args[0].ToUpper();
-                        switch (command) {
-                            case "DISCONNECT": {
-                                await reader.ReadLineAsync();
-                                throw new Exception();
+                                var command = args[0].ToUpper();
+                                switch (command) {
+                                    case "DISCONNECT": {
+                                        await reader.ReadLineAsync();
+                                        throw new Exception();
+                                    }
+                                    case "PUB": {
+                                        var topic               = args[1];
+                                        var contentLengthHeader = (await reader.ReadLineAsync())!.ToLower();
+                                        await reader.ReadLineAsync();
+                                        var length = int.Parse(contentLengthHeader.Split("content-length: ", StringSplitOptions.RemoveEmptyEntries)[0]);
+                                        var charArray = new char[length];
+                                        await reader.ReadAsync(charArray);
+                                        await reader.ReadLineAsync();
+                                        await reader.ReadLineAsync();
+                                        var message = new string(charArray);
+                                        _queueManager.Publish(topic, message);
+                                        break;
+                                    }
+                                    case "SUB": {
+                                        var topic = args[1];
+                                        _queueManager.Subscribe(topic, client);
+                                        break;
+                                    }
+                                    case "UNSUB": {
+                                        var topic = args[1];
+                                        _queueManager.Unsubscribe(topic, client);
+                                        break;
+                                    }
+                                }
                             }
-                            case "PUB": {
-                                var topic               = args[1];
-                                var contentLengthHeader = (await reader.ReadLineAsync())!.ToLower();
-                                await reader.ReadLineAsync();
-                                var length    = int.Parse(contentLengthHeader.Split("content-length: ", StringSplitOptions.RemoveEmptyEntries)[0]);
-                                var charArray = new char[length];
-                                await reader.ReadAsync(charArray);
-                                await reader.ReadLineAsync();
-                                await reader.ReadLineAsync();
-                                var message = new string(charArray);
-                                _queueManager.Publish(topic, message);
-                                break;
-                            }
-                            case "SUB": {
-                                var topic = args[1];
-                                _queueManager.Subscribe(topic, client);
-                                break;
-                            }
-                            case "UNSUB": {
-                                var topic = args[1];
-                                _queueManager.Unsubscribe(topic, client);
-                                break;
-                            }
+                        } catch (Exception) {
+                            _queueManager.UnsubscribeFromAll(client);
+                            // Console.WriteLine("Client disconnected");
+                            client.Dispose();
                         }
-                    }
-                } catch (Exception) {
-                    _queueManager.UnsubscribeFromAll(client);
-                    // Console.WriteLine("Client disconnected");
+                    });
                 }
             });
         }
