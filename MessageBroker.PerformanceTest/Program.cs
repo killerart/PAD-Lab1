929fe964a0881ba5a07a160850085ec44e07885d
killerart <theemee.work@gmail.com>
2021-09-22 02:34:55 +0300

    performance testing

---
+++ MessageBroker.PerformanceTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using MessageBroker.Client.Grpc;
+using MessageBroker.Client.Socket;
+using MessageBroker.TestClient;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace MessageBroker.PerformanceTest {
+    class Program {
+        const int ClientCount = 16;
+
+        static void Main(string[] args) {
+            TestSocket();
+            // TestGrpc();
+        }
+
+        static void TestSocket() {
+            var factory = ClientFactory.Create(name: "socket_factory",
+                                               clientCount: ClientCount + 2,
+                                               initClient: (_, _) => Task.FromResult(new SocketMessageBrokerClient("localhost", 9876)));
+
+            var step = Step.Create("step",
+                                   clientFactory: factory,
+                                   timeout: TimeSpan.FromSeconds(20),
+                                   execute: async context => {
+                                       var messageBrokerClient = context.Client;
+                                       await messageBrokerClient.Publish(new Message {
+                                           Id   = 5,
+                                           Text = "Hello",
+                                           Time = DateTime.Now
+                                       });
+
+                                       return Response.Ok();
+                                   });
+
+            var scenario = ScenarioBuilder.CreateScenario("socket_test", step)
+                                          .WithWarmUpDuration(TimeSpan.FromSeconds(20))
+                                          .WithLoadSimulations(Simulation.KeepConstant(ClientCount, TimeSpan.FromMinutes(1)));
+
+            NBomberRunner.RegisterScenarios(scenario).Run();
+        }
+
+        static void TestGrpc() {
+            var factory = ClientFactory.Create(name: "grpc_factory",
+                                               clientCount: ClientCount * 100,
+                                               initClient: (_, _) => Task.FromResult(new GrpcMessageBrokerClient("localhost", 9876)));
+
+            var step = Step.Create("step",
+                                   clientFactory: factory,
+                                   timeout: TimeSpan.FromSeconds(20),
+                                   execute: async context => {
+                                       var messageBrokerClient = context.Client;
+                                       await messageBrokerClient.Publish(new Message {
+                                           Id   = 5,
+                                           Text = "Hello",
+                                           Time = DateTime.Now
+                                       });
+
+                                       return Response.Ok();
+                                   });
+
+            var scenario = ScenarioBuilder.CreateScenario("grpc_test", step)
+                                          .WithWarmUpDuration(TimeSpan.FromSeconds(20))
+                                          .WithLoadSimulations(Simulation.KeepConstant(ClientCount, TimeSpan.FromMinutes(1)));
+
+            NBomberRunner.RegisterScenarios(scenario).Run();
+        }
+    }
+}
--- MessageBroker.TestClient/Program.cs
+++ MessageBroker.TestClient/Program.cs
@@ -1,37 +1,46 @@
 ﻿using System;
 using System.Text.Json;
 using System.Threading.Tasks;
+using MessageBroker.Client.Core;
 using MessageBroker.Client.Grpc;
 using MessageBroker.Client.Socket;
 
 namespace MessageBroker.TestClient {
     class Program {
         static async Task Main() {
+            var clients = new IMessageBrokerClient[8];
+            for (var i = 0; i < clients.Length; i++) {
+                clients[i] = new SocketMessageBrokerClient("127.0.0.1", 9876);
+                // clients[i] = new GrpcMessageBrokerClient("127.0.0.1", 9876);
+                await clients[i].Subscribe<Message>(HandleMessageEvent);
+                clients[i].StartListening();
+            }
+
             // using var messageBrokerClient = new SocketMessageBrokerClient("127.0.0.1", 9876);
-            using var messageBrokerClient = new GrpcMessageBrokerClient("127.0.0.1", 9876);
-
-            await messageBrokerClient.Subscribe<Message>(HandleMessageEvent);
-
-            messageBrokerClient.StartListening();
-
-            await messageBrokerClient.Publish(new Message {
-                Id   = 5,
-                Text = "Hello",
-                Time = DateTime.Now
-            });
+            // // using var messageBrokerClient = new GrpcMessageBrokerClient("127.0.0.1", 9876);
+            //
+            // messageBrokerClient.StartListening();
+            //
+            // await messageBrokerClient.Publish(new Message {
+            //     Id   = 5,
+            //     Text = "Hello",
+            //     Time = DateTime.Now
+            // });
 
             Console.ReadKey(true);
+            foreach (var client in clients) {
+                await client.Unsubscribe<Message>(HandleMessageEvent);
 
-            await messageBrokerClient.Unsubscribe<Message>(HandleMessageEvent);
-
-            await messageBrokerClient.Disconnect();
+                await client.Disconnect();
+                client.Dispose();
+            }
         }
 
         private static void HandleMessageEvent(object message) {
-            Console.WriteLine(JsonSerializer.Serialize(message,
-                                                       new JsonSerializerOptions {
-                                                           WriteIndented = true
-                                                       }));
+            // Console.WriteLine(JsonSerializer.Serialize(message,
+            //                                            new JsonSerializerOptions {
+            //                                                WriteIndented = true
+            //                                            }));
         }
     }
 
